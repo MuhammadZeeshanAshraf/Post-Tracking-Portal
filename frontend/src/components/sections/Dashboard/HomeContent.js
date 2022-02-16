8b75502a2a0fcdeadda60dfdefb3649b0f76efff
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2022-02-16 22:07:55 +0500

    added import process table

--- frontend/src/components/sections/Dashboard/HomeContent.js
+++ frontend/src/components/sections/Dashboard/HomeContent.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import Breadcrumb from '../Datatable/Breadcrumb';
+import React, { useEffect, useState } from 'react';
 import "../../../assets/css/homeContent.css"
+import axios from 'axios';
+import ImportProcess from './ImportProcess';
+import Breadcrumb from '../Roles/Breadcrumb';
 
 const HomeContent = () => {
+    const [importProcesses, setImportProcesses] = useState([]);
+    
+    const getImportProcess = () =>{
+        axios.get('/table/all-import-process')
+        .then(function (response) {
+            console.log(response.data.data)
+            setImportProcesses(response.data.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log("Error in getting roles data");
+        })
+    }
+    
+    useEffect(()=>{
+        const fetchData = async () => {
+            await getImportProcess();
+         }
+         fetchData();
+    }, []);
+
     return (
         <div className="ms-content-wrapper">
             <div className="col-md-12">
-                {/* <Breadcrumb /> */}
+                <Breadcrumb />
                 <div className="chartsMain">
                     <div  className="chartRow d-flex">
                         <div className="chartContent color1">
@@ -30,7 +53,7 @@ const HomeContent = () => {
                         </div>
                     </div>
                 </div>
-                
+                <ImportProcess dataList={importProcesses} />
             </div>
         </div>
     );
