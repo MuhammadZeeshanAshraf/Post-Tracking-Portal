76b1351f6a071f660bab23eba55174f46dd14b66
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2022-02-20 02:52:21 +0500

    updated comingSoon page

--- frontend/src/components/sections/ComingSoon/Content.js
+++ frontend/src/components/sections/ComingSoon/Content.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
+import CMimage from '../../../assets/img/costic/comingSoon.png'
 
 class Content extends Component {
     render() {
         return (
-            <div className="ms-body ms-primary-theme">
-                <main className="body-content ms-coming-soon">
-                    <div className="ms-content-wrapper">
-                        {/* <h1>We Are Coming Soon</h1> */}
-                        {/* <p className="ms-duration">Our Exciting Website is Under Construction</p> */}
-                    </div>
-                </main>
+            <div className="ms-content-wrapper ms-coming-soon p-0">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+                    <img src={CMimage} />
+                </div>
             </div>
-
         );
     }
 }
