4f88436411df7b31985a15e1e4aa3c359d20d429
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2022-02-20 01:17:59 +0500

    Removed quickbar from every screen

--- frontend/src/components/pages/UploadFile.jsx
+++ frontend/src/components/pages/UploadFile.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Quickbar from '../layouts/Quickbar';
 import Sidenavigation from '../layouts/Sidenavigation';
 import Topnavigation from '../layouts/Topnavigation';
 import UploadContent from '../sections/Upload/Cotnent';
@@ -12,7 +11,6 @@ function UploadFile(props) {
             <Topnavigation />
             <UploadContent/>
         </main>
-        <Quickbar />
     </div>
     );
 }
